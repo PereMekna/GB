71b8d5b79576a982e44ed6433755d2aca182b2f8
PereMekna <dupont.louis4@gmail.com>
2016-08-04 17:14:27 +0200

    some styling

--- index.php
+++ index.php
@@ -4,10 +4,13 @@
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
+  <meta name="theme-color" content="#f57f17">
   <title>Grand Bo</title>
 
   <!-- CSS  -->
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href='https://fonts.googleapis.com/css?family=IM+Fell+Great+Primer' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Merriweather' rel='stylesheet' type='text/css'>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/css/materialize.min.css">
   <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
 </head>
@@ -15,35 +18,32 @@
   <div class="navbar-fixed">
     <nav class="white" role="navigation">
       <div class="nav-wrapper container">
-        <a id="logo-container" href="#" class="brand-logo">Logo</a>
+        <a id="logo-container" href="#" class="brand-logo"><img src="img/logo_grand_bornand.png" width="125px"/></a>
         <ul class="right hide-on-med-and-down">
-          <li><a href="#">Navbar Link</a></li>
+          <li><a href="#">Nous contacter</a></li>
         </ul>
 
         <ul id="nav-mobile" class="side-nav">
-          <li><a href="#">Navbar Link</a></li>
+          <li><a href="#">Nous contacter</a></li>
         </ul>
-        <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
+        <a href="#" data-activates="nav-mobile" class="button-collapse yellow-text text-darken-4"><i class="material-icons">menu</i></a>
       </div>
     </nav>
   </div>
 
   <div id="index-banner" class="parallax-container">
     <div class="section no-pad-bot">
-      <div class="container">
+      <div class="container filter">
         <br><br>
-        <h1 class="header center deep-orange-text">Studio au Grand-Bornand</h1>
+        <h1 class="header center">Studio au Grand-Bornand</h1>
         <div class="row center">
-          <h5 class="header col s12 grey">Un studio coquet, pour 4 personnes, au pied des pistes au Grand-Bornand Chinaillon.</h5>
-        </div>
-        <div class="row center">
-          <a href="http://materializecss.com/getting-started.html" id="download-button" class="btn-large waves-effect waves-light indigo lighten-1">Nous contacter</a>
+          <h5 class="header col s12">Un studio coquet, pour 4 personnes, au pied des pistes au Grand-Bornand Chinaillon.</h5>
         </div>
         <br><br>
 
       </div>
     </div>
-    <div class="parallax"><img src="img/7.jpg"></div>
+    <div class="parallax"><img src="img/8.jpg"></div>
   </div>
 
 
@@ -52,21 +52,45 @@
 
       <!--   Icon Section   -->
       <div class="row">
-        <div class="col s12 m6">
+        <div class="col s12 m4">
           <div class="icon-block">
-            <h2 class="center brown-text"><i class="material-icons">flash_on</i></h2>
+            <h2 class="center yellow-text text-darken-4"><i class="material-icons">store</i></h2>
             <h5 class="center">Location studio</h5>
 
             <p class="light">Studio pour 4 personnes, au pied des pistes, orienté plein sud, avec balcon. Un canapé convertible et lit superposés, télévision et cuisine aménagée.</p>
           </div>
         </div>
 
-        <div class="col s12 m6">
+        <div class="col s12 m4">
           <div class="icon-block">
-            <h2 class="center brown-text"><i class="material-icons">group</i></h2>
-            <h5 class="center">Contactez nous</h5>
+            <h2 class="center yellow-text text-darken-4"><i class="material-icons">group</i></h2>
+            <h5 class="center">Contact et réservations</h5>
 
             <p class="light"><i class="material-icons">contact_phone</i> 06 81 23 54 24<br /><i class="material-icons">email</i> <a href="mailto:gegemomo.dupont@laposte.net">gegemomo.dupont@laposte.net</a></p>
+          </div>
+        </div>
+
+        <div class="col s12 m4">
+          <div class="icon-block">
+            <h2 class="center yellow-text text-darken-4"><i class="material-icons">thumb_up</i></h2>
+            <h5 class="center">A proximité</h5>
+
+            
+              <ul class="list-style-circle">
+                <li>
+                  <p class="light">Office de tourisme et épicerie à 500m</p>
+                </li>
+                <li>
+                  <p class="light">Vente de produits locaux dans le village (fromages, charcuterie)</p>
+                </li>
+                <li>
+                  <p class="light">École de Ski à 100m</p>
+                </li>
+                <li>
+                  <p class="light">Départs télésiéges à 150m</p>
+                </li>
+              </ul>
+            </p>
           </div>
         </div>
       </div>
@@ -76,55 +100,64 @@
 
   <div class="container">
       <div class="row">
-        <div class="col l6 m12 s12 center">
-          <div class="slider">
-            <ul class="slides">
-              <li>
-                <img class="materialboxed" src="img/1.jpg"> <!-- random image -->
-                <div class="caption left-align">
-                  <h3 class="black-text">Salle de séjour</h3>
-                  <h5 class="grey-text text-lighten-3">Télévision, canapé convertible 2 personnes</h5>
-                </div>
-              </li>
-              <li>
-                <img class="materialboxed"  src="img/2.jpg"> <!-- random image -->
-                <div class="caption left-align">
-                  <h3>Cuisine aménagée</h3>
-                  <h5 class="light grey-text text-lighten-3">Four multi-fonctions, plaques inductions</h5>
-                </div>
-              </li>
-              <li>
-                <img class="materialboxed"  src="img/3.jpg"> <!-- random image -->
-                <div class="caption right-align">
-                  <h3>Balcon</h3>
-                  <h5 class="light grey-text text-lighten-3">Vue sur les pistes</h5>
-                </div>
-              </li>
-              <li>
-                <img class="materialboxed"  src="img/4.jpg"> <!-- random image -->
-                <div class="caption center-align">
-                  <h3>Deux lits superposés</h3>
-                  <h5 class="light grey-text text-lighten-3">Largeur : 80 cm</h5>
-                </div>
-              </li>
-              <li>
-                <img class="materialboxed"  src="img/6.jpg"> <!-- random image -->
-                <div class="caption left-align">
-                  <h3>Salle de bains</h3>
-                  <h5 class="light grey-text text-lighten-3">Baignoire/douche</h5>
-                </div>
-              </li>
-              <li>
-                <img class="materialboxed"  src="img/5.jpg"> <!-- random image -->
-                <div class="caption left-align">
-                  <h3>Vue depuis les pistes</h3>
-                </div>
-              </li>
-            </ul>
+        <div class="col l6 m12 s12">
+          <div class="icon-block">
+            <h2 class="center yellow-text text-darken-4"><i class="material-icons">picture_in_picture</i></h2>
+            <h5 class="center">Galerie photo</h5>
+
+            <div class="slider">
+              <ul class="slides">
+                <li>
+                  <img class="materialboxed" src="img/1.jpg"> <!-- random image -->
+                  <div class="caption center-align">
+                    <h3>Salle de séjour</h3>
+                    <h5 class="grey-text text-lighten-3">Télévision, canapé convertible 2 personnes</h5>
+                  </div>
+                </li>
+                <li>
+                  <img class="materialboxed"  src="img/2.jpg"> <!-- random image -->
+                  <div class="caption center-align">
+                    <h3>Cuisine aménagée</h3>
+                    <h5 class="light grey-text text-lighten-3">Four multi-fonctions, plaques inductions</h5>
+                  </div>
+                </li>
+                <li>
+                  <img class="materialboxed"  src="img/3.jpg"> <!-- random image -->
+                  <div class="caption center-align">
+                    <h3>Balcon</h3>
+                    <h5 class="light grey-text text-lighten-3">Vue sur les pistes</h5>
+                  </div>
+                </li>
+                <li>
+                  <img class="materialboxed"  src="img/4.jpg"> <!-- random image -->
+                  <div class="caption center-align">
+                    <h3>Deux lits superposés</h3>
+                    <h5 class="light grey-text text-lighten-3">Largeur : 80 cm</h5>
+                  </div>
+                </li>
+                <li>
+                  <img class="materialboxed"  src="img/6.jpg"> <!-- random image -->
+                  <div class="caption center-align">
+                    <h3>Salle de bains</h3>
+                    <h5 class="light grey-text text-lighten-3">Baignoire/douche</h5>
+                  </div>
+                </li>
+                <li>
+                  <img class="materialboxed"  src="img/5.jpg"> <!-- random image -->
+                  <div class="caption center-align">
+                    <h3>Vue depuis les pistes</h3>
+                  </div>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
         <div class="col m12 s12 l6">
+          <div class="icon-block">
+            <h2 class="center yellow-text text-darken-4"><i class="material-icons">location_on</i></h2>
+            <h5 class="center">Accès</h5>
           <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2773.062672478328!2d6.449268614861855!3d45.97000610757202!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x478bf724f34e6cbd%3A0xeca4bc104480d292!2sVoie+Communale+N%C2%B06+de+la+Place+aux+Gettiers%2C+74450+Le+Grand-Bornand!5e0!3m2!1sfr!2sfr!4v1470147497689" width="100%" height="400px" frameborder="0" style="border:0" allowfullscreen></iframe>
+          </div>
         </div>
       </div>
 
@@ -133,17 +166,10 @@
 
 
   <div class="parallax-container valign-wrapper">
-    <div class="section no-pad-bot">
-      <div class="container">
-        <div class="row center">
-          <h5 class="header col s12 light">A modern responsive front-end framework based on Material Design</h5>
-        </div>
-      </div>
-    </div>
-    <div class="parallax"><img src="img/5.jpg" alt="Unsplashed background img 3"></div>
+    <div class="parallax"><img src="img/7.jpg" alt="Unsplashed background img 3"></div>
   </div>
 
-  <footer class="page-footer teal">
+  <footer class="page-footer yellow darken-4">
     <div class="container">
       <div class="row">
         <div class="col l6 s12">
@@ -174,7 +200,7 @@
     </div>
     <div class="footer-copyright">
       <div class="container">
-      Made by <a class="brown-text text-lighten-3" href="http://materializecss.com">Materialize</a>
+      Made with <a class="brown-text text-lighten-3" href="http://materializecss.com">Materialize</a>
       </div>
     </div>
   </footer>
@@ -184,7 +210,6 @@
   <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
   <!-- Compiled and minified JavaScript -->
   <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/js/materialize.min.js"></script>
-  <script src="js/unslider-min.js"></script> <!-- but with the right path! -->
   <script src="js/init.js"></script>
   <script>
     $(document).ready(function(){
